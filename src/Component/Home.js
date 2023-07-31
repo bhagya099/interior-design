d1d591c167e109ab7d384d4ea667bcd3defa99fd
Bhagyashree <bhagya099@gmail.com>
2023-07-31 17:17:56 +1000

    Adding swiper images

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,21 +1,40 @@
 import React from "react";
 import IMAGES from "../image/img";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCoverflow,
+} from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import { auto } from "@popperjs/core";
 
 export default function Home() {
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      pagination={{ clickable: true }}
+      effect={"coverflow"}
+      grabCursor={true}
+      centeredSlides={true}
+      loop={true}
+      modules={[EffectCoverflow, Navigation, Pagination, Scrollbar, A11y]}
+      slidesPerView={"auto"}
+      navigation={{
+        nextEl: "",
+      }}
+      pagination={{ el: "", clickable: true }}
       scrollbar={{ draggable: true }}
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+      }}
     >
       {IMAGES.Home_image.map((slide, i) => (
         <SwiperSlide key={i}>
